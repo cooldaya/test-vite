9e5207affa6c1e8cab28d8d602a42c90118bf864
zhangpengjiang <3317414758@qq.com>
2024-12-19 14:49:58 +0800

    '测试webcomponents'

--- src/cus-els/Tip1.ce.vue
+++ src/cus-els/Tip1.ce.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="h-[100px] w-[100px] bg-[red] pointer-events-auto">测试 {{ store.test_data }}</div>
+  <div class="h-[100px] w-[100px] bg-[red] pointer-events-auto">测试 {{ store.test_data }}
+    <kt-video src="https://www.w3school.com.cn/i/movie.mp4" />
+  </div>
 </template>
 <script setup>
 import store from "@/store.js";  
