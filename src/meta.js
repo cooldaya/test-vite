3a0668f55c3f0b76f85558fae8dbb963dd2f1f2c
zhangpengjiang <3317414758@qq.com>
2024-12-27 13:04:21 +0800

    调整a-verify.js引入位置

--- src/meta.js
+++ src/meta.js
@@ -2,8 +2,8 @@ import { reactive } from "vue";
 
 // 全局共享的响应式数据
 const meta = reactive({
-  isPixel: true,
-  globalLoading:true
+  isPixel: false,
+  globalLoading:false
 });
 
 export default meta;
